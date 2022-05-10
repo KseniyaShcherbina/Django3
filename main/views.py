930b1ff66afbe085888ee898dc1a528b70528fdc
Hello <ksnosha@mail.ru>
2022-05-10 11:34:26 +0300

    новая стр. About

--- main/views.py
+++ main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(request)
+
+def about(request):
+    return HttpResponse("<h4>About</h4>")
