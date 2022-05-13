b62b6dfdcd9d8ea815bca9515075eaf7ccb0e1d3
Hello <ksnosha@mail.ru>
2022-05-13 15:08:17 +0300

    подключение forms

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,13 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 class TaskForm(ModelForm):
 
     class Meta:
         model = Task
         fields = ["title", "task"]
+        widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите имя'}),
+            "task": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите отзыв'})
+        }
 
 form = TaskForm()
